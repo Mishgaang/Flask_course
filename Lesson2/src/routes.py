c871257f2dac6db2e0b3f65eba7030056c7bba39
Михаил Пахоменков <misha-pahomenkov@yandex.ru>
2023-02-07 18:03:48 +0300

    Created populated_db

--- Lesson2/src/routes.py
+++ Lesson2/src/routes.py
@@ -3,6 +3,7 @@ from Lesson2.src.resources.actors import ActorListApi
 from Lesson2.src.resources.aggregations import AggregationApi
 from Lesson2.src.resources.auth import AuthRegister, AuthLogin
 from Lesson2.src.resources.films import FilmListApi
+from Lesson2.src.resources.populate_db import PopulateDB
 from Lesson2.src.resources.smoke import Smoke
 
 api.add_resource(Smoke, '/smoke', strict_slashes=False)
@@ -11,3 +12,4 @@ api.add_resource(ActorListApi, '/actors', '/actors/<uuid>', strict_slashes=False
 api.add_resource(AggregationApi, '/aggregations', strict_slashes=False)
 api.add_resource(AuthRegister, '/register', strict_slashes=False)
 api.add_resource(AuthLogin, '/login', strict_slashes=False)
+api.add_resource(PopulateDB, '/populate_db', strict_slashes=False)
