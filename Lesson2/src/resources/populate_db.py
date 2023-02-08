f6e25d244c1a48c16990df3a2398252ef2cbc3a8
Михаил Пахоменков <misha-pahomenkov@yandex.ru>
2023-02-08 11:49:32 +0300

    Added class with Threading

--- Lesson2/src/resources/populate_db.py
+++ Lesson2/src/resources/populate_db.py
@@ -1,4 +1,5 @@
 import datetime
+import threading
 
 import bs4
 import requests
@@ -17,6 +18,12 @@ def convert_time(time: str) -> float:
 class PopulateDB(Resource):
     url = 'https://www.imdb.com/'
 
+    headers = {
+        'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.1.1114 Yowser/2.5 Safari/537.36",
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'referer': 'https://www.google.ru/',
+    }
+
     def post(self):
         t0 = datetime.datetime.now()
         films_urls = self.get_films_urls()
@@ -29,7 +36,7 @@ class PopulateDB(Resource):
     def get_films_urls(self):
         print('Getting film urls', flush=True)
         url = self.url + 'chart/top/'
-        resp = requests.get(url)
+        resp = requests.get(url, headers=self.headers)
         resp.raise_for_status()
 
         html = resp.text
@@ -43,12 +50,80 @@ class PopulateDB(Resource):
         for url in film_urls:
             url = self.url + url
             print(f'Getting a detailed info about the film - {url}')
-            film_content = requests.get(url)
+            film_content = requests.get(url, headers=self.headers)
             film_content.raise_for_status()
 
             html = film_content.text
             soup = bs4.BeautifulSoup(html, features='html.parser')
-            title, _ = soup.find('div', class_='originalTitle').text.split('(')
+            title = soup.find('td', class_='titleColumn').text
+            rating = float(soup.find('div', class_='ratingValue').strong.text)
+            description = soup.find('div', class_='summary_text').text.strip()
+            title_bar = soup.find('div', class_='titleBar').text.strip()
+            title_content = title_bar.split('\n')
+            release_date, _ = title_content[-1].split('(')
+            release_date = datetime.datetime.strptime(release_date.strip(), '%d %B %Y')
+            length = convert_time(soup.find('div', class_='subtext').time.text.strip())
+            print(f'Received information about - {title}', flush=True)
+            films_to_create.append(
+                {
+                    'title': title,
+                    'rating': rating,
+                    'description': description,
+                    'release_date': release_date,
+                    'length': length,
+                    'distributed_by': 'Warner Bros. Pictures'
+                }
+            )
+        return films_to_create
+
+
+class PopulateDBThreaded(Resource):
+    url = 'https://www.imdb.com/'
+
+    headers = {
+        'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.1.1114 Yowser/2.5 Safari/537.36",
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'referer': 'https://www.google.ru/',
+    }
+
+    def post(self):
+        threads = []
+        films_to_create = []
+        t0 = datetime.datetime.now()
+        films_urls = self.get_films_urls()
+        for film_url in films_urls:
+            threads.append(threading.Thread(target=self.parse_films, args=(film_url, films_to_create), daemon=True))
+        [t.start() for t in threads]
+        [t.join() for t in threads]
+        created_films = self.populate_db_with_films(films_to_create)
+
+        dt = datetime.datetime.now() - t0
+        print(f'Done in {dt.total_seconds():.2f} sec.')
+        return {'message': f'Database were populated with {created_films} films'}, 201
+
+    def get_films_urls(self):
+        print('Getting film urls', flush=True)
+        url = self.url + 'chart/top/'
+        resp = requests.get(url, headers=self.headers)
+        resp.raise_for_status()
+
+        html = resp.text
+        soup = bs4.BeautifulSoup(html, features='html.parser')
+        movie_containers = soup.find_all('td', class_='posterColumn')
+        movie_links = [movie.a.attrs['href'] for movie in movie_containers][:10]
+        return movie_links
+
+    def parse_films(self, film_urls):
+        films_to_create = []
+        for url in film_urls:
+            url = self.url + url
+            print(f'Getting a detailed info about the film - {url}')
+            film_content = requests.get(url, headers=self.headers)
+            film_content.raise_for_status()
+
+            html = film_content.text
+            soup = bs4.BeautifulSoup(html, features='html.parser')
+            title = soup.find('td', class_='titleColumn').text
             rating = float(soup.find('div', class_='ratingValue').strong.text)
             description = soup.find('div', class_='summary_text').text.strip()
             title_bar = soup.find('div', class_='titleBar').text.strip()
--- Lesson2/src/routes.py
+++ Lesson2/src/routes.py
@@ -3,7 +3,7 @@ from Lesson2.src.resources.actors import ActorListApi
 from Lesson2.src.resources.aggregations import AggregationApi
 from Lesson2.src.resources.auth import AuthRegister, AuthLogin
 from Lesson2.src.resources.films import FilmListApi
-from Lesson2.src.resources.populate_db import PopulateDB
+from Lesson2.src.resources.populate_db import PopulateDB, PopulateDBThreaded
 from Lesson2.src.resources.smoke import Smoke
 
 api.add_resource(Smoke, '/smoke', strict_slashes=False)
@@ -13,3 +13,4 @@ api.add_resource(AggregationApi, '/aggregations', strict_slashes=False)
 api.add_resource(AuthRegister, '/register', strict_slashes=False)
 api.add_resource(AuthLogin, '/login', strict_slashes=False)
 api.add_resource(PopulateDB, '/populate_db', strict_slashes=False)
+api.add_resource(PopulateDBThreaded, '/populate_db_threaded', strict_slashes=False)
